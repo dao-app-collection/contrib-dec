a43b2dc37a2e6c99cb5a64c758a0c14268fc049e
Daniel Treviño Bergman <danielivert@gmail.com>
2021-12-07 09:27:49 +0100

    fix: config

--- packages/frontend/src/lib/config.ts
+++ packages/frontend/src/lib/config.ts
@@ -4,8 +4,8 @@ const config = {
   NETWORK: (process.env.NEXT_PUBLIC_NETWORK as unknown as SupportedNetworks) ?? 'rinkeby',
   ENVIRONMENT: process.env.ENVIRONMENT ?? 'dev',
   ROUNDED_DECIMALS: 4,
-  CONFIG_CAT_SDK_KEY: 'xYjZCIkJi0eABaTu6QgigA/c-u2N7zb0EGnXQhvsjdsnQ',
-  SITE_URL: process.env.SITE_URL ?? 'https://www.example.com/',
+  CONFIG_CAT_SDK_KEY: '',
+  SITE_URL: process.env.SITE_URL ?? 'https://hackaton.contrib.at/',
 }
 
 const appConfig = {
